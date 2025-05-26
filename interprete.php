b2d81d6b0754c50d89c056f7155a6d6d3656b441
Adrian Alberto Luty <adrianalbertoluty@gmail.com>
2025-05-26 11:34:25 -0300

    Create interprete.php

---
+++ interprete.php
@@ -0,0 +1,35 @@
+<?php
+
+$acumulador = 0;
+
+function interpretar($linea, &$acumulador) {
+    $linea = trim($linea);
+
+    if (preg_match('/^decime "(.*)"$/', $linea, $matches)) {
+        echo $matches[1] . PHP_EOL;
+    } elseif (preg_match('/^metele (\d+)$/', $linea, $matches)) {
+        $acumulador += (int)$matches[1];
+    } elseif (preg_match('/^sacale (\d+)$/', $linea, $matches)) {
+        $acumulador -= (int)$matches[1];
+    } elseif ($linea === "cuantohay") {
+        echo "Hay $acumulador" . PHP_EOL;
+    } elseif ($linea === "rajá") {
+        echo "Listo loco, nos vimos." . PHP_EOL;
+        exit;
+    } elseif ($linea === "") {
+        // Ignorar líneas vacías
+    } else {
+        echo "No te entiendo: $linea" . PHP_EOL;
+    }
+}
+
+$archivo = $argv[1] ?? null;
+if (!$archivo || !file_exists($archivo)) {
+    echo "Pasame un archivo .lunf para interpretar." . PHP_EOL;
+    exit(1);
+}
+
+$lineas = file($archivo);
+foreach ($lineas as $linea) {
+    interpretar($linea, $acumulador);
+}
